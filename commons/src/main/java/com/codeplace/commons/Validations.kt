d10724662ed4eff57b5d87a83f55e91c22bdee7c
Vanshi <preprojectdev@gmail.com>
2020-04-15 00:43:21 +0530

    merge

--- commons/src/main/java/com/codeplace/commons/Validations.kt
+++ commons/src/main/java/com/codeplace/commons/Validations.kt
@@ -1,10 +1,23 @@
 package com.codeplace.commons
 
-import android.content.Context
-import android.widget.Toast
+import android.text.TextUtils
+import android.util.Patterns
+
 
 class Validations {
-    fun sowToast(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
+    fun isValidEmail(email: String): Boolean {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun isEmpty(data: String): Boolean {
+        return TextUtils.isEmpty(data)
+    }
+
+    fun lengthCheck(data: String, length: Int): Boolean {
+        return !TextUtils.isEmpty(data) && data.length > length
+    }
+
+    fun rangeCheck(data: String, min: Int, max: Int): Boolean {
+        return !TextUtils.isEmpty(data) && data.length > min && max < data.length
     }
 }
